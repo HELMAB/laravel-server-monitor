589d5b485785cf374a8fedf6c43993098176a64c
Hel Mab <mabhelitc@gmail.com>
2021-02-24 13:03:51 +0700

    update ui

--- resources/views/server-monitor.blade.php
+++ resources/views/server-monitor.blade.php
@@ -8,10 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @livewire('server-monitor')
-                {{--<div class="p-6 bg-white border-b border-gray-200">
-                    // content
-                </div>--}}
+                <div class="p-6 bg-white border-b border-gray-200">
+                    @livewire('server-monitor')
+                </div>
             </div>
         </div>
     </div>
