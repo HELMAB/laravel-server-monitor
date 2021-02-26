3a25d158a9bc071d25aa0601508b18337bc04e75
HEL Mab <mabhelitc@gmail.com>
2021-02-26 20:32:57 +0700

    update code

--- app/Http/Livewire/MonitorDatatable.php
+++ app/Http/Livewire/MonitorDatatable.php
@@ -5,10 +5,11 @@ namespace App\Http\Livewire;
 use App\Models\Monitor;
 use Mediconesystems\LivewireDatatables\Column;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
-use Mediconesystems\LivewireDatatables\NumberColumn;
 
 class MonitorDatatable extends LivewireDatatable
 {
+    protected $series = 0;
+
     public $model = Monitor::class;
 
     public $exportable = true;
@@ -19,11 +20,15 @@ class MonitorDatatable extends LivewireDatatable
 
     public function columns()
     {
+        $this->series = $this->page > 1 ? ($this->page - 1) * $this->perPage : 0;
+
         return [
-            NumberColumn::name('id')->label('ID'),
+            Column::callback(['id'], function ($id) {
+                return ++$this->series;
+            })->label('NO'),
             Column::name('url')->label('DOMAIN'),
             Column::callback(['uptime_status'], function ($uptime_status) {
-                 return view('status', ['status' => $uptime_status]);
+                return view('status', ['status' => $uptime_status]);
             })->label('STATUS'),
             Column::callback(['uptime_check_failure_reason'], function ($uptime_check_failure_reason) {
                 return $uptime_check_failure_reason != '' ? $uptime_check_failure_reason : '_';
